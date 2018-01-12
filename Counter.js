b2e62765b30657a43e2bf5d7aa96519b15b89468
Pedro <buesimples@gmail.com>
2018-01-12 10:41:19 +0000

    adding async action

--- Counter.js
+++ Counter.js
@@ -2,16 +2,21 @@ import { Connect } from "redux-zero/preact";
 
 import actions from "./actions";
 
-const mapToProps = ({ count }) => ({ count });
+const mapToProps = ({ count, loading, payload }) => ({ count, loading, payload });
 
 export default () => (
   <Connect mapToProps={mapToProps} actions={actions}>
-    {({ count, increment, decrement }) => (
+    {({ count, loading, payload, increment, decrement, getTodos }) => (
       <div>
-        <h1>{count}</h1>
+        <h1>{count} - {loading.toString()} </h1>
+
+        <p>getTodos response (not actually todos :p):</p>
+        <pre>{ JSON.stringify(payload, null, 2) }</pre>
+
         <div>
           <button onClick={decrement}>decrement</button>
           <button onClick={increment}>increment</button>
+          <button onClick={getTodos}>getTodos</button>
         </div>
       </div>
     )}
--- actions.js
+++ actions.js
@@ -1,4 +1,14 @@
-export default store => ({
+export default ({setState}) => ({
   decrement: state => ({ count: state.count - 1 }),
-  increment: state => ({ count: state.count + 1 })
+  increment: state => ({ count: state.count + 1 }),
+  getTodos() {
+    setState({ loading: true });
+
+    return fetch("https://jsonplaceholder.typicode.com/posts/1")
+      .then(res => res.json())
+      .then(payload => {
+        return { payload, loading: false }
+      })
+      .catch(error => ({ error, loading: false }))
+  }
 });
