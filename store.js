991147624d6876db606d135c95072628216b1513
Pedro <buesimples@gmail.com>
2018-01-12 10:16:36 +0000

    connecting to dev tools

--- store.js
+++ store.js
@@ -1,5 +1,10 @@
 import createStore from "redux-zero";
+import { applyMiddleware } from 'redux-zero/middleware';
+import { connect } from 'redux-zero/devtools';
 
 const initialState = { count: 1 };
 
-export default createStore(initialState);
+const middlewares = connect ? applyMiddleware(connect(initialState)): [];
+const store = createStore(initialState, middlewares);
+
+export default store;
